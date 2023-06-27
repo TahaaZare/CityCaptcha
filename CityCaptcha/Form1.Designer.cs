5f6df9bc7bb87a859818d6c96580075c89371bef
Taha Zare <81828931+TahaaZare@users.noreply.github.com>
2023-06-28 00:13:32 +0330

    Commit Project

---
+++ CityCaptcha/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace CityCaptcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            p_ardebil = new PictureBox();
+            p_boshehr = new PictureBox();
+            p_esfarayen = new PictureBox();
+            p_gorgan = new PictureBox();
+            p_khorasanR = new PictureBox();
+            p_tabriz = new PictureBox();
+            p_khozestan = new PictureBox();
+            p_yazd = new PictureBox();
+            p_tehran = new PictureBox();
+            rbtn_yazd = new RadioButton();
+            rbtn_tabriz = new RadioButton();
+            rbtn_khorasanR = new RadioButton();
+            rbtn_tehran = new RadioButton();
+            rbtn_khozestan = new RadioButton();
+            rbtn_golestan = new RadioButton();
+            rbtn_ardebil = new RadioButton();
+            rbtn_esfarayen = new RadioButton();
+            rbtn_boshehr = new RadioButton();
+            label1 = new Label();
+            score = new Label();
+            ((System.ComponentModel.ISupportInitialize)p_ardebil).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)p_boshehr).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)p_esfarayen).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)p_gorgan).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)p_khorasanR).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)p_tabriz).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)p_khozestan).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)p_yazd).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)p_tehran).BeginInit();
+            SuspendLayout();
+            // 
+            // p_ardebil
+            // 
+            p_ardebil.Image = Properties.Resources.ardebil;
+            p_ardebil.Location = new Point(21, 332);
+            p_ardebil.Name = "p_ardebil";
+            p_ardebil.Size = new Size(258, 150);
+            p_ardebil.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_ardebil.TabIndex = 0;
+            p_ardebil.TabStop = false;
+            p_ardebil.Click += p_ardebil_Click;
+            // 
+            // p_boshehr
+            // 
+            p_boshehr.Image = Properties.Resources.boshehr;
+            p_boshehr.Location = new Point(285, 332);
+            p_boshehr.Name = "p_boshehr";
+            p_boshehr.Size = new Size(258, 150);
+            p_boshehr.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_boshehr.TabIndex = 1;
+            p_boshehr.TabStop = false;
+            p_boshehr.Click += p_boshehr_Click;
+            // 
+            // p_esfarayen
+            // 
+            p_esfarayen.Image = Properties.Resources.esfarayen;
+            p_esfarayen.Location = new Point(549, 332);
+            p_esfarayen.Name = "p_esfarayen";
+            p_esfarayen.Size = new Size(258, 150);
+            p_esfarayen.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_esfarayen.TabIndex = 2;
+            p_esfarayen.TabStop = false;
+            p_esfarayen.Click += p_esfarayen_Click;
+            // 
+            // p_gorgan
+            // 
+            p_gorgan.Image = Properties.Resources.gorgan;
+            p_gorgan.Location = new Point(21, 488);
+            p_gorgan.Name = "p_gorgan";
+            p_gorgan.Size = new Size(258, 150);
+            p_gorgan.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_gorgan.TabIndex = 3;
+            p_gorgan.TabStop = false;
+            p_gorgan.Click += p_gorgan_Click;
+            // 
+            // p_khorasanR
+            // 
+            p_khorasanR.Image = Properties.Resources.khorasanR;
+            p_khorasanR.Location = new Point(285, 488);
+            p_khorasanR.Name = "p_khorasanR";
+            p_khorasanR.Size = new Size(258, 150);
+            p_khorasanR.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_khorasanR.TabIndex = 4;
+            p_khorasanR.TabStop = false;
+            p_khorasanR.Click += p_khorasanR_Click;
+            // 
+            // p_tabriz
+            // 
+            p_tabriz.Image = Properties.Resources.tabriz;
+            p_tabriz.Location = new Point(549, 488);
+            p_tabriz.Name = "p_tabriz";
+            p_tabriz.Size = new Size(258, 150);
+            p_tabriz.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_tabriz.TabIndex = 5;
+            p_tabriz.TabStop = false;
+            p_tabriz.Click += p_tabriz_Click;
+            // 
+            // p_khozestan
+            // 
+            p_khozestan.Image = Properties.Resources.khozestan;
+            p_khozestan.Location = new Point(549, 644);
+            p_khozestan.Name = "p_khozestan";
+            p_khozestan.Size = new Size(258, 150);
+            p_khozestan.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_khozestan.TabIndex = 6;
+            p_khozestan.TabStop = false;
+            p_khozestan.Click += p_khozestan_Click;
+            // 
+            // p_yazd
+            // 
+            p_yazd.Image = Properties.Resources.yazd;
+            p_yazd.Location = new Point(285, 644);
+            p_yazd.Name = "p_yazd";
+            p_yazd.Size = new Size(258, 150);
+            p_yazd.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_yazd.TabIndex = 7;
+            p_yazd.TabStop = false;
+            p_yazd.Click += p_yazd_Click;
+            // 
+            // p_tehran
+            // 
+            p_tehran.Image = Properties.Resources.tehran;
+            p_tehran.Location = new Point(21, 644);
+            p_tehran.Name = "p_tehran";
+            p_tehran.Size = new Size(258, 150);
+            p_tehran.SizeMode = PictureBoxSizeMode.StretchImage;
+            p_tehran.TabIndex = 8;
+            p_tehran.TabStop = false;
+            p_tehran.Click += p_tehran_Click;
+            // 
+            // rbtn_yazd
+            // 
+            rbtn_yazd.AutoSize = true;
+            rbtn_yazd.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_yazd.Location = new Point(674, 19);
+            rbtn_yazd.Name = "rbtn_yazd";
+            rbtn_yazd.Size = new Size(133, 25);
+            rbtn_yazd.TabIndex = 9;
+            rbtn_yazd.TabStop = true;
+            rbtn_yazd.Text = "امیر چخماق - یزد";
+            rbtn_yazd.UseVisualStyleBackColor = true;
+            rbtn_yazd.CheckedChanged += rbtn_yazd_CheckedChanged;
+            // 
+            // rbtn_tabriz
+            // 
+            rbtn_tabriz.AutoSize = true;
+            rbtn_tabriz.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_tabriz.Location = new Point(682, 50);
+            rbtn_tabriz.Name = "rbtn_tabriz";
+            rbtn_tabriz.Size = new Size(125, 25);
+            rbtn_tabriz.TabIndex = 10;
+            rbtn_tabriz.TabStop = true;
+            rbtn_tabriz.Text = "الاداغ لار - تبریز";
+            rbtn_tabriz.UseVisualStyleBackColor = true;
+            rbtn_tabriz.CheckedChanged += rbtn_tabriz_CheckedChanged;
+            // 
+            // rbtn_khorasanR
+            // 
+            rbtn_khorasanR.AutoSize = true;
+            rbtn_khorasanR.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_khorasanR.Location = new Point(588, 112);
+            rbtn_khorasanR.Name = "rbtn_khorasanR";
+            rbtn_khorasanR.Size = new Size(219, 25);
+            rbtn_khorasanR.TabIndex = 11;
+            rbtn_khorasanR.TabStop = true;
+            rbtn_khorasanR.Text = "خارجرد خواف - خراسان رضوی";
+            rbtn_khorasanR.UseVisualStyleBackColor = true;
+            rbtn_khorasanR.CheckedChanged += rbtn_khorasanR_CheckedChanged;
+            // 
+            // rbtn_tehran
+            // 
+            rbtn_tehran.AutoSize = true;
+            rbtn_tehran.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_tehran.Location = new Point(657, 81);
+            rbtn_tehran.Name = "rbtn_tehran";
+            rbtn_tehran.Size = new Size(150, 25);
+            rbtn_tehran.TabIndex = 12;
+            rbtn_tehran.TabStop = true;
+            rbtn_tehran.Text = " موزه آبگینه - تهران";
+            rbtn_tehran.UseVisualStyleBackColor = true;
+            rbtn_tehran.CheckedChanged += rbtn_tehran_CheckedChanged;
+            // 
+            // rbtn_khozestan
+            // 
+            rbtn_khozestan.AutoSize = true;
+            rbtn_khozestan.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_khozestan.Location = new Point(672, 143);
+            rbtn_khozestan.Name = "rbtn_khozestan";
+            rbtn_khozestan.Size = new Size(135, 25);
+            rbtn_khozestan.TabIndex = 13;
+            rbtn_khozestan.TabStop = true;
+            rbtn_khozestan.Text = "کارون - خوزستان";
+            rbtn_khozestan.UseVisualStyleBackColor = true;
+            rbtn_khozestan.CheckedChanged += rbtn_khozestan_CheckedChanged;
+            // 
+            // rbtn_golestan
+            // 
+            rbtn_golestan.AutoSize = true;
+            rbtn_golestan.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_golestan.Location = new Point(684, 174);
+            rbtn_golestan.Name = "rbtn_golestan";
+            rbtn_golestan.Size = new Size(123, 25);
+            rbtn_golestan.TabIndex = 14;
+            rbtn_golestan.TabStop = true;
+            rbtn_golestan.Text = "النگدره - گرگان";
+            rbtn_golestan.UseVisualStyleBackColor = true;
+            rbtn_golestan.CheckedChanged += rbtn_golestan_CheckedChanged;
+            // 
+            // rbtn_ardebil
+            // 
+            rbtn_ardebil.AutoSize = true;
+            rbtn_ardebil.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_ardebil.Location = new Point(659, 205);
+            rbtn_ardebil.Name = "rbtn_ardebil";
+            rbtn_ardebil.Size = new Size(148, 25);
+            rbtn_ardebil.TabIndex = 15;
+            rbtn_ardebil.TabStop = true;
+            rbtn_ardebil.Text = "موزه چینی - اردبیل";
+            rbtn_ardebil.UseVisualStyleBackColor = true;
+            rbtn_ardebil.CheckedChanged += rbtn_ardebil_CheckedChanged;
+            // 
+            // rbtn_esfarayen
+            // 
+            rbtn_esfarayen.AutoSize = true;
+            rbtn_esfarayen.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_esfarayen.Location = new Point(672, 236);
+            rbtn_esfarayen.Name = "rbtn_esfarayen";
+            rbtn_esfarayen.Size = new Size(135, 25);
+            rbtn_esfarayen.TabIndex = 16;
+            rbtn_esfarayen.TabStop = true;
+            rbtn_esfarayen.Text = "بلقیس - اسفراین";
+            rbtn_esfarayen.UseVisualStyleBackColor = true;
+            rbtn_esfarayen.CheckedChanged += rbtn_esfarayen_CheckedChanged;
+            // 
+            // rbtn_boshehr
+            // 
+            rbtn_boshehr.AutoSize = true;
+            rbtn_boshehr.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            rbtn_boshehr.Location = new Point(602, 267);
+            rbtn_boshehr.Name = "rbtn_boshehr";
+            rbtn_boshehr.Size = new Size(205, 25);
+            rbtn_boshehr.TabIndex = 17;
+            rbtn_boshehr.TabStop = true;
+            rbtn_boshehr.Text = "مدرسه سعادت آباد - بوشهر";
+            rbtn_boshehr.UseVisualStyleBackColor = true;
+            rbtn_boshehr.CheckedChanged += rbtn_boshehr_CheckedChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 81);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 21);
+            label1.TabIndex = 18;
+            label1.Text = "امتیاز شمـا : ";
+            // 
+            // score
+            // 
+            score.AutoSize = true;
+            score.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            score.Location = new Point(107, 86);
+            score.Name = "score";
+            score.Size = new Size(0, 21);
+            score.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(828, 813);
+            Controls.Add(score);
+            Controls.Add(label1);
+            Controls.Add(rbtn_boshehr);
+            Controls.Add(rbtn_esfarayen);
+            Controls.Add(rbtn_ardebil);
+            Controls.Add(rbtn_golestan);
+            Controls.Add(rbtn_khozestan);
+            Controls.Add(rbtn_tehran);
+            Controls.Add(rbtn_khorasanR);
+            Controls.Add(rbtn_tabriz);
+            Controls.Add(rbtn_yazd);
+            Controls.Add(p_tehran);
+            Controls.Add(p_yazd);
+            Controls.Add(p_khozestan);
+            Controls.Add(p_tabriz);
+            Controls.Add(p_khorasanR);
+            Controls.Add(p_gorgan);
+            Controls.Add(p_esfarayen);
+            Controls.Add(p_boshehr);
+            Controls.Add(p_ardebil);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)p_ardebil).EndInit();
+            ((System.ComponentModel.ISupportInitialize)p_boshehr).EndInit();
+            ((System.ComponentModel.ISupportInitialize)p_esfarayen).EndInit();
+            ((System.ComponentModel.ISupportInitialize)p_gorgan).EndInit();
+            ((System.ComponentModel.ISupportInitialize)p_khorasanR).EndInit();
+            ((System.ComponentModel.ISupportInitialize)p_tabriz).EndInit();
+            ((System.ComponentModel.ISupportInitialize)p_khozestan).EndInit();
+            ((System.ComponentModel.ISupportInitialize)p_yazd).EndInit();
+            ((System.ComponentModel.ISupportInitialize)p_tehran).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox p_ardebil;
+        private PictureBox p_boshehr;
+        private PictureBox p_esfarayen;
+        private PictureBox p_gorgan;
+        private PictureBox p_khorasanR;
+        private PictureBox p_tabriz;
+        private PictureBox p_khozestan;
+        private PictureBox p_yazd;
+        private PictureBox p_tehran;
+        private RadioButton rbtn_yazd;
+        private RadioButton rbtn_tabriz;
+        private RadioButton rbtn_khorasanR;
+        private RadioButton rbtn_tehran;
+        private RadioButton rbtn_khozestan;
+        private RadioButton rbtn_golestan;
+        private RadioButton rbtn_ardebil;
+        private RadioButton rbtn_esfarayen;
+        private RadioButton rbtn_boshehr;
+        private Label label1;
+        private Label score;
+    }
+}
---
+++ CityCaptcha/Form1.cs
@@ -0,0 +1,355 @@
+﻿namespace CityCaptcha
+{
+    public partial class Form1 : Form
+    {
+        int imageCode, cityCode;
+        int score_point = 9;
+        public Form1()
+        {
+            InitializeComponent();
+            score.Text = score_point.ToString();
+        }
+
+        private void p_ardebil_Click(object sender, EventArgs e)
+        {
+            imageCode = 1;
+            if (imageCode == 1 && cityCode == 1)
+            {
+                p_ardebil.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void rbtn_ardebil_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 1;
+        }
+
+        private void p_boshehr_Click(object sender, EventArgs e)
+        {
+            imageCode = 2;
+            if (imageCode == 2 && cityCode == 2)
+            {
+                p_boshehr.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void rbtn_boshehr_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 2;
+        }
+
+        private void p_esfarayen_Click(object sender, EventArgs e)
+        {
+            imageCode = 3;
+            if (imageCode == 3 && cityCode == 3)
+            {
+                p_esfarayen.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void rbtn_esfarayen_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 3;
+        }
+
+        private void p_gorgan_Click(object sender, EventArgs e)
+        {
+            imageCode = 4;
+            if (imageCode == 4 && cityCode == 4)
+            {
+                p_gorgan.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void rbtn_golestan_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 4;
+        }
+
+        private void p_khorasanR_Click(object sender, EventArgs e)
+        {
+            imageCode = 5;
+            if (imageCode == 5 && cityCode == 5)
+            {
+                p_khorasanR.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void rbtn_khorasanR_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 5;
+        }
+
+        private void p_tabriz_Click(object sender, EventArgs e)
+        {
+            imageCode = 6;
+            if (imageCode == 6 && cityCode == 6)
+            {
+                p_tabriz.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void rbtn_tabriz_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 6;
+        }
+
+        private void p_tehran_Click(object sender, EventArgs e)
+        {
+            imageCode = 7;
+            if (imageCode == 7 && cityCode == 7)
+            {
+                p_tehran.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void rbtn_tehran_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 7;
+        }
+
+        private void p_yazd_Click(object sender, EventArgs e)
+        {
+            imageCode = 8;
+            if (imageCode == 8 && cityCode == 8)
+            {
+                p_yazd.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void p_khozestan_Click(object sender, EventArgs e)
+        {
+            imageCode = 9;
+            if (imageCode == 9 && cityCode == 9)
+            {
+                p_khozestan.Visible = false;
+                if (p_ardebil.Visible == false &&
+                    p_boshehr.Visible == false &&
+                    p_gorgan.Visible == false &&
+                    p_khozestan.Visible == false &&
+                    p_tabriz.Visible == false &&
+                    p_khorasanR.Visible == false &&
+                    p_esfarayen.Visible == false &&
+                    p_tehran.Visible == false &&
+                    p_yazd.Visible == false)
+                {
+                    MessageBox.Show($"شما برنده شدید امتیاز شما : {score_point}");
+                }
+            }
+            else
+            {
+                if (score_point != 0)
+                {
+                    score_point -= 1;
+                    score.Text = score_point.ToString();
+                }
+                else if (score_point <= 0)
+                {
+                    MessageBox.Show("باختی !");
+                }
+            }
+        }
+
+        private void rbtn_yazd_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 8;
+        }
+
+        private void rbtn_khozestan_CheckedChanged(object sender, EventArgs e)
+        {
+            cityCode = 9;
+        }
+    }
+}
